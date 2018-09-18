f8aed5e1496d8fe3723cbf0d76b383c69ae6b6d9
Alexander Schranz <alexander@sulu.io>
2018-09-18 15:00:20 +0200

    code style fixes

--- config/bundles.php
+++ config/bundles.php
@@ -44,9 +44,9 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     // Admin
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true, 'admin' => true],
-    Sulu\Bundle\AdminBundle\SuluAdminBundle::class => ['all' => true,'admin' => true],
-    Sulu\Bundle\CollaborationBundle\SuluCollaborationBundle::class => ['all' => true,'admin' => true],
-    Sulu\Bundle\PreviewBundle\SuluPreviewBundle::class => ['all' => true,'admin' => true],
+    Sulu\Bundle\AdminBundle\SuluAdminBundle::class => ['all' => true, 'admin' => true],
+    Sulu\Bundle\CollaborationBundle\SuluCollaborationBundle::class => ['all' => true, 'admin' => true],
+    Sulu\Bundle\PreviewBundle\SuluPreviewBundle::class => ['all' => true, 'admin' => true],
     // Website
-    Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true,'website' => true],
+    Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
 ];
