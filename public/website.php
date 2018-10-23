038dff6a71c1df4b7344111172746ee36e1fd326
Alexander Schranz <alexander@sulu.io>
2018-10-23 13:11:49 +0200

    enable http kernel method override in website file

--- public/website.php
+++ public/website.php
@@ -62,6 +62,10 @@ if ($env !== 'dev') {
     $kernel = $kernel->getHttpCache();
 }
 
+// When using the HttpCache, you need to call the method in your front controller
+// instead of relying on the configuration parameter
+// https://symfony.com/doc/3.4/reference/configuration/framework.html#http-method-override
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
