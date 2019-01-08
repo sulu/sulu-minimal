5f24c9dddaea2c113212391b10c7186d445dce5e
Alexander Schranz <alexander@sulu.io>
2019-01-08 11:26:01 +0100

    fix ide problems with reading of the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpackConfig = require('./vendor/sulu/sulu/webpack.config.js');
 
 module.exports = (env, argv) => {
-    if (!env) {
-        env = {};
-    }
+    env = env ? env : {};
+    argv = argv ? argv : {};
 
     env.root_path = __dirname;
+
     const config = webpackConfig(env, argv);
+
+    // For adding custom js file uncomment the following line:
+    // config.entry.unshift(path.resolve(__dirname, 'assets/admin/index.js'));
 
     return config;
 };
