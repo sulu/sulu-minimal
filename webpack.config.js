fbf98b91027320c1abee8a993ac25cceeba4ba2a
Alexander Schranz <alexander@sulu.io>
2019-01-08 10:03:13 +0100

    Fix accidently cleanup of build folder by IDE

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 const webpackConfig = require('./vendor/sulu/sulu/webpack.config.js');
 
-module.exports = webpackConfig;
+module.exports = (env, argv) => {
+    if (!env) {
+        env = {};
+    }
+
+    env.root_path = __dirname;
+    const config = webpackConfig(env, argv);
+
+    return config;
+};
