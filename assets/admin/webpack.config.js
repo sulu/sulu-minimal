32b29caa642df76a69dacf4bcc17000e039e9cb1
Alexander Schranz <alexander@sulu.io>
2019-02-20 19:17:49 +0100

    Move admin js folder to assets/admin

--- assets/admin/webpack.config.js
+++ assets/admin/webpack.config.js
@@ -1,15 +1,16 @@
-const webpackConfig = require('./vendor/sulu/sulu/webpack.config.js');
+const path = require('path');
+const webpackConfig = require('./../../vendor/sulu/sulu/webpack.config.js');
 
 module.exports = (env, argv) => {
     env = env ? env : {};
     argv = argv ? argv : {};
 
-    env.root_path = __dirname;
+    env.root_path = path.resolve(__dirname, '..', '..');
 
     const config = webpackConfig(env, argv);
 
     // For adding custom admin js file uncomment the following line:
-    // config.entry.unshift(path.resolve(__dirname, 'assets/admin/index.js'));
+    // config.entry.unshift(path.resolve(__dirname, 'index.js'));
 
     return config;
 };
