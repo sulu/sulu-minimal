2578bbe9cabc728523c9e10dac3f7f93059271e3
Daniel Rotter <daniel.rotter@gmail.com>
2019-08-27 11:31:33 +0200

    Add more polyfills to support IE11

--- assets/admin/index.js
+++ assets/admin/index.js
@@ -3,11 +3,17 @@ import 'regenerator-runtime/runtime';
 import 'url-search-params-polyfill';
 import 'whatwg-fetch';
 import 'core-js/fn/array/includes';
+import 'core-js/fn/array/find';
 import 'core-js/fn/array/find-index';
 import 'core-js/fn/array/fill';
 import 'core-js/fn/array/from';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/object/values';
 import 'core-js/fn/promise';
+import 'core-js/fn/string/code-point-at';
+import 'core-js/fn/string/includes';
 import 'core-js/fn/symbol';
+import 'unorm';
 
 // Bundles
 import {startAdmin} from 'sulu-admin-bundle';
