19900a4f6a3ef23adb29c6cb3711b0a5f9497f6d
Daniel Rotter <daniel.rotter@gmail.com>
2019-08-28 09:54:52 +0200

    Move files to assets folder

--- assets/admin/webpack.config.js
+++ assets/admin/webpack.config.js
@@ -2,16 +2,17 @@
 /* eslint-disable import/no-nodejs-modules*/
 /* eslint-disable no-undef */
 const path = require('path');
-const webpackConfig = require('./vendor/sulu/sulu/webpack.config.js');
+const webpackConfig = require('../../vendor/sulu/sulu/webpack.config.js');
 
 module.exports = (env, argv) => {
     env = env ? env : {};
     argv = argv ? argv : {};
 
-    env.root_path = __dirname;
+    env.project_root_path = path.resolve(__dirname, '..', '..');
+    env.node_modules_path = path.resolve(__dirname ,'node_modules');
 
     const config = webpackConfig(env, argv);
-    config.entry = path.resolve(__dirname, 'assets/admin/index.js');
+    config.entry = path.resolve(__dirname, 'index.js');
 
     return config;
 };
